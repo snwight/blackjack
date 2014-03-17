dd7faadc05b9ef19e85b7ab2d31a92cdce826a11
Steve Wight <northwight@gmail.com>
2014-03-17 01:12:18 -0700

    Pretty much operative. I need to update the README.

--- blackjack.go
+++ blackjack.go
@@ -2,109 +2,404 @@ package main
 
 import (
 	"fmt"
+	"github.com/hoisie/web"
+	"github.com/kr/pretty"
 	"math/rand"
 	"time"
-	"github.com/hoisie/web"
 )
 
-
 /*
-    data type declarations
- */
+   data type declarations
+*/
+
+const HouseMinFunds = 5
+const HouseMinBet = 5
+const HouseDealerStay = 17
+const HousePayoffFactor = 2.0    // bet + 100%
+const HouseBlackjackFactor = 2.5 // bet + 150%
+const HouseDfltDeckCount = 2
+const BaseDeckLen = 52
+const BlackjackScore = 21
+const (
+	Win = iota
+	Loss
+	Push
+	Blackjack
+)
+
+// note single value per card
 type Card struct {
-    value int
-    name string
+	value int
+	name  string
 }
-
 type Hand []Card
-type Deck []Card
+
+// participants are only single player and dealer
 type Player struct {
-	name string
-	hand Hand
+	hand  Hand
+	funds float64
+	bet   float64
+}
+type Dealer struct {
+	hand  Hand
+	funds float64
+	gains float64
 }
 
+type Deck []Card
 
 /*
-    "global" data objects
- */
+   "global" data objects
+*/
 
-// initialized sorted deck
-var fullDeck = Deck {
-	{1, "CA"}, {2, "C2"}, {3, "C3"}, {4, "C4"}, {5, "C5"}, {6, "C6"}, {7, "C7"}, 
-	{8, "C8"}, {9, "C9"}, {10, "C10"}, {10, "CJ"}, {10, "CQ"}, {10, "CK"},
-	{1, "DA"}, {2, "D2"}, {3, "D3"}, {4, "D4"}, {5, "D5"}, {6, "D6"}, {7, "D7"}, 
-	{8, "D8"}, {9, "D9"}, {10, "D10"}, {10, "DJ"}, {10, "DQ"}, {10, "DK"},
-	{1, "HA"}, {2, "H2"}, {3, "H3"}, {4, "H4"}, {5, "H5"}, {6, "H6"}, {7, "H7"}, 
-	{8, "H8"}, {9, "H9"}, {10, "H10"}, {10, "HJ"}, {10, "HQ"}, {10, "HK"},
-	{1, "SA"}, {2, "S2"}, {3, "S3"}, {4, "S4"}, {5, "S5"}, {6, "S6"}, {7, "S7"}, 
-	{8, "S8"}, {9, "S9"}, {10, "S10"}, {10, "SJ"}, {10, "SQ"}, {10, "SK"},
+// a single initialized sorted deck - aces defaulted to value 1
+var fullDeck = Deck{
+	{1, "AC"}, {2, "2C"}, {3, "3C"}, {4, "4C"}, {5, "5C"}, {6, "6C"}, {7, "7C"},
+	{8, "8C"}, {9, "9C"}, {10, "10C"}, {10, "JC"}, {10, "QC"}, {10, "KC"},
+	{1, "AD"}, {2, "2D"}, {3, "3D"}, {4, "4D"}, {5, "5D"}, {6, "6D"}, {7, "7D"},
+	{8, "8D"}, {9, "9D"}, {10, "10D"}, {10, "JD"}, {10, "QD"}, {10, "KD"},
+	{1, "AH"}, {2, "2H"}, {3, "3H"}, {4, "4H"}, {5, "5H"}, {6, "6H"}, {7, "7H"},
+	{8, "8H"}, {9, "9H"}, {10, "10H"}, {10, "JH"}, {10, "QH"}, {10, "KH"},
+	{1, "AS"}, {2, "2S"}, {3, "3S"}, {4, "4S"}, {5, "5S"}, {6, "6S"}, {7, "7S"},
+	{8, "8S"}, {9, "9S"}, {10, "10S"}, {10, "JS"}, {10, "QS"}, {10, "KS"},
 }
-var deck = fullDeck
+var deck Deck
+var deckCount = HouseDfltDeckCount
 
-var dealer = Player{"dealer", nil}
-var me = Player{"me", nil}
-
+var dealer = Dealer{nil, 1000000, 0}
+var me = Player{nil, 0, 0}
 
 /*
-    load the callbacks and start the webgo server
- */
+   load the callbacks and start the webgo server
+*/
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	// player api
 	web.Get("/deal", deal)
 	web.Get("/hit", hit)
-	web.Post("/bet", bet)
+	web.Get("/stay", stay)
+	web.Get("/bet", bet)
+	web.Get("/hand", hand)
+	web.Get("/deposit", deposit)
+	web.Get("/funds", funds)
+	// admin api
+	web.Pot("/show_deck", show_deck)
+	web.Get("/size_deck", size_deck)
 	web.Run("0.0.0.0:9999")
 }
 
-
 /*
-    API callbacks
- */
+   API callbacks
+*/
 
-// curl http://localhost:9999/deal
-func deal() Hand {
-	// for now reload and shuffle deck for every hand!
-	shuffle()
+// curl http://localhost:9999/deal[?reload=y]
+func deal(ctx *web.Context) string {
+
+	// ensure a bet has been placed
+	if me.bet == 0 {
+		return fmt.Sprintln("You haven't placed a bet yet!\n")
+	}
+
+	// play with a full deck/s
+	if deck == nil || ctx.Params["reload"] == "y" {
+		reload()
+	}
+
+	// trust in garbage collection
 	dealer.hand = nil
 	me.hand = nil
-	dealer.hand = append(dealer.hand, deck[0], deck[2])
-	me.hand = append(me.hand, deck[1], deck[3])
- 	deck = deck[4:]
-	fmt.Printf("dealer showing: %+v, you have: %+v\n", dealer.hand, me.hand)
-	return me.hand
+
+	// NB: hardcoded rule, implicit ordering! weak of me.
+	dlr1 := pop()
+	me1 := pop()
+	dlr2 := pop()
+	me2 := pop()
+
+	// update everyone's hands
+	dealer.hand = append(dealer.hand, dlr1, dlr2)
+	me.hand = append(me.hand, me1, me2)
+
+	// check for a blackjack now, after first pair is dealt
+	if me.hand.sum() == BlackjackScore {
+		win := settle(Blackjack)
+		return fmt.Sprintln("You win %.2f parsohns with a blackjack!\n", win)
+	}
+
+	return pretty.Sprintf("Dealer showing: %# v\nYou have: %# v\nSum: %d\n\n",
+		dealer.hand[0], me.hand, me.hand.sum())
+
 }
 
 // curl http://localhost:9999/hit
-func hit() Hand {
-	card := deck[0]
-	deck = deck[1:]
+func hit() string {
+
+	// make sure initial deal has happened
+	if me.hand == nil {
+		return fmt.Sprintln("No hits before the opening deal!\n")
+	}
+
+	// ensure a bet has been placed
+	if me.bet == 0 {
+		return fmt.Sprintln("You haven't placed a bet yet!\n")
+	}
+
+	// pull top card off and add to hand
+	card := pop()
 	me.hand = append(me.hand, card)
-	fmt.Printf("hit: %+v\n", card)
-	return me.hand
+
+	// check for a bust and cleanup accounts if so
+	sum := me.hand.sum()
+	if sum > BlackjackScore {
+		loss := settle(Loss)
+		return fmt.Sprintln("You are bust, loss of %f parsohns\n", loss)
+	}
+
+	return pretty.Sprintf("Dealer showing: %# v\nYou have: %# v\nSum: %d\n\n",
+		dealer.hand[0], me.hand, me.hand.sum())
+
 }
 
-// curl --data "amount=XXX" http://localhost:9999/bet
-func bet(ctx *web.Context) string { 
-	return "bet: " + ctx.Params["amount"] + "\n"
+// curl http://localhost:9999/hit
+func stay() string {
+
+	// ensure a bet has been placed
+	if me.bet == 0 {
+		return fmt.Sprint("You haven't placed a bet yet!\n")
+	}
+
+	// the dealer now finishes his hand
+	dealer_wrap()
+
+	// otherwise we conclude our deliberations
+	myScore := me.hand.sum()
+	dealerScore := dealer.hand.sum()
+	switch {
+	case dealerScore > BlackjackScore || myScore > dealerScore:
+		// dealer is bust! player wins
+		win := settle(Win)
+		return fmt.Sprintf("You win %.2f parsohns of space cash\n\n", win)
+	case myScore == dealerScore:
+		// it's a push - return bet to player funds
+		settle(Push)
+		return fmt.Sprintf("Push!\n\n")
+	default:
+		// player lost - bet goes to dealer funds
+		loss := settle(Loss)
+		return fmt.Sprintf("You lose %.2f parsohns: %d to dealer's %d\n\n",
+			loss, myScore, dealerScore)
+	}
+
 }
 
+// curl http://localhost:9999/bet?amount=XXX
+func bet(ctx *web.Context) string {
+	var bet float64
+	num, _ := fmt.Sscanf(ctx.Params["amount"], "%f", &bet)
+
+	// if no parameters - treat this as a query
+	if num == 0 {
+		return fmt.Sprintf("Your current bet: %.2f\n\n", me.bet)
+	}
+
+	// check the bookeeping before accepting bet
+	if bet < HouseMinBet {
+		return fmt.Sprintf("Bet %.2f is under house minimum (%.2f)\n\n",
+			HouseMinBet, bet)
+	}
+	if bet > me.funds {
+		return fmt.Sprintf("Bet %.2f is above your available funds (%.2f)\n\n",
+			bet, me.funds)
+	}
+
+	// move bet to 'table'
+	me.bet += bet
+	me.funds -= bet
+
+	return fmt.Sprintf("Your bet: %.2f parsohns of space cash\n\n", bet)
+
+}
+
+// curl http://localhost:9999/deposit[?amount=XXX]
+func deposit(ctx *web.Context) string {
+	var deposit float64
+
+	// check for format problems
+	num, err := fmt.Sscanf(ctx.Params["amount"], "%f", &deposit)
+	if num != 1 {
+		return fmt.Sprintf("Deposit failed, %# v\n\n", err)
+	}
+
+	// transfer the funds
+	me.funds += deposit
+
+	return fmt.Sprintf("Deposited %.2f\n\n", deposit)
+
+}
+
+// curl http://localhost:9999/hand
+func hand() string {
+
+	// make sure initial deal has happened
+	if me.hand == nil {
+		return fmt.Sprintln("No hand before the opening deal!\n")
+	}
+
+	// report player status
+	return pretty.Sprintf("Dealer showing: %# v\nYou have: %# v\nSum: %d\n\n",
+		dealer.hand[0], me.hand, me.hand.sum())
+
+}
+
+// curl http://localhost:9999/funds
+func funds() string {
+
+	// report player available funds
+	return fmt.Sprintf("Your remaining funds: %.2f parsohns of space cash\n\n",
+		me.funds)
+
+}
 
 /*
-    utility functions
- */
+   ADMIN API
+*/
 
-// single deck game is default - append to slice as needed
-func shuffle() Deck {
-	deck = fullDeck
+// curl --data auth="titanoboa" http://localhost:9999/show_deck
+// XXX very serious security hole! for debugging only XXX
+func show_deck(ctx *web.Context) string {
+	// check our "security"
+	if ctx.Params["auth"] != "titanoboa" {
+		return fmt.Sprintf("Incorrect auth\n")
+	}
+
+	return pretty.Sprintf("Current state of deck: %# v\n\n", deck)
+}
+
+// curl http://localhost:9999/size_deck?count=XXX
+func size_deck(ctx *web.Context) string {
+
+	// update the global deck count
+	num, err := fmt.Sscanf(ctx.Params["count"], "%d", &deckCount)
+	if num != 1 {
+		return fmt.Sprintf("Deck resize failed, %# v\n\n", err)
+	}
+
+	// reallocate deck at new size
+	reload()
+
+	return pretty.Sprintf("New state of deck: %# v\n\n", deck)
+
+}
+
+/*
+   utility functions
+*/
+
+// reinitialize deck/s with deckCount fresh packs
+func reload() {
+
+	// 'free' the global deck
+	deck = nil
+
+	// allocate a new [possibly multi-deck] deck
+	deckLen := deckCount * BaseDeckLen
+	deck = make(Deck, deckLen)
+
+	// init by cut and paste using our template deck
+	for i := 0; i < deckLen; i += BaseDeckLen {
+		copy(deck[i:], fullDeck)
+	}
+
+	// shuffle because model is sorted in order
+	deck.shuffle()
+	return
+
+}
+
+// implement Algorithm P, Knuth/Durstenfeld
+func (deck Deck) shuffle() Deck {
+
+	// TL/DR: swap every element with a random earlier element
 	l := len(deck)
-	// Algorithm P, Knuth/Durstenfeld
 	for i := l - 1; i > 0; i-- {
 		j := rand.Intn(l - i)
 		tmp := deck[j]
 		deck[j] = deck[i]
 		deck[i] = tmp
 	}
-	fmt.Printf("shuffled deck: %+v\n", deck)
 	return deck
+
 }
 
+// implement Algorithm P, Knuth/Durstenfeld
+func pop() Card {
+
+	// pop card and update deck slice
+	card := deck[0]
+	deck = deck[1:]
+	return card
+
+}
+
+// add up this hand
+func (hand Hand) sum() int {
+
+	ace, sum := 0, 0
+	for i := 0; i < len(hand); i++ {
+		// count the aces while summing
+		if hand[i].value == 11 {
+			ace++
+		}
+		sum += hand[i].value
+	}
+
+	// swap an 11 for a 1 if we're going to bust
+	if ace > 0 && sum > Blackjack {
+		sum -= 10
+		if ace == 2 {
+			fmt.Println("two aces! we should split!/n")
+		}
+	}
+	return sum
+
+}
+
+// sort out the funds transfers and clear per-game state (hands and bets)
+func settle(result int) float64 {
+	net := 0.0
+
+	switch result {
+	case Win:
+		net = me.bet * HousePayoffFactor
+		me.funds += net
+		dealer.funds -= net
+	case Blackjack:
+		net = me.bet * HouseBlackjackFactor
+		me.funds += net
+		dealer.funds -= net
+	case Loss:
+		net = me.bet
+		dealer.funds += net
+	case Push:
+		me.funds += me.bet
+	}
+
+	// discard the dealt hands
+	dealer.hand = nil
+	me.hand = nil
+	me.bet = 0
+	return net
+}
+
+// after player/s have stayed or busted, this is called to complete the dealer's hand
+func dealer_wrap() {
+
+	// implement the "hit 'till 17 or bust" rule if necessary
+	for dealer.hand.sum() < HouseDealerStay {
+		card := pop()
+		dealer.hand = append(dealer.hand, card)
+	}
+
+	// yes, we may be bust, press on regardless
+	return
+
+}
